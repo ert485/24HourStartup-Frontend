d8a23a53b18935b8962d450a28128adce469fae5
Erik Tetland <erik.tetland@gmail.com>
2021-02-06 14:29:48 -0600

    merge attempt

--- frontend-v2/src/app/home/home.component.ts
+++ frontend-v2/src/app/home/home.component.ts
@@ -1,14 +1,40 @@
-import { Component, Injector, ChangeDetectionStrategy } from '@angular/core';
-import { AppComponentBase } from '@shared/app-component-base';
-import { appModuleAnimation } from '@shared/animations/routerTransition';
+import { ChangeDetectionStrategy, Component, Injector } from "@angular/core";
+import { appModuleAnimation } from "@shared/animations/routerTransition";
+import { AppComponentBase } from "@shared/app-component-base";
+import { Observable } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { BackendService } from "../backend.service";
 
 @Component({
-  templateUrl: './home.component.html',
+  templateUrl: "./home.component.html",
   animations: [appModuleAnimation()],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent extends AppComponentBase {
+  backendService: BackendService;
   constructor(injector: Injector) {
     super(injector);
+    this.backendService = injector.get(BackendService);
   }
+
+  documents$: Observable<any>;
+
+  ngOnInit(): void {
+    this.documents$ = this.backendService
+      .listDocuments()
+      .pipe(
+        catchError(() => [
+          [
+            "document1",
+            "document2",
+            "document3",
+            "document4",
+            "document5",
+            "document6",
+            "document7",
+          ],
+        ])
+      );
+  }
+  newDocument() {}
 }
